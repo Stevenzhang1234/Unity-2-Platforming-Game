1b9083d6db616895b2b3296aefa980acc497b2ad
Steven13257384096 <70672458+Steven13257384096@users.noreply.github.com>
2020-09-30 18:42:54 -0700

    unity2

--- Assets/Scripts/playerManager.cs
+++ Assets/Scripts/playerManager.cs
@@ -152,7 +152,7 @@ public class playerManager : MonoBehaviour
     }
 
 
-    private void OnTriggerErnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.GetComponent<Collectable>() != null)
         {
